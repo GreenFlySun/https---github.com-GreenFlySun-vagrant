bcac666e460f9e927d50cb70fefce5f6294cb3d6
Kuro <tolikzaa2000@gmail.com>
2022-02-21 18:44:54 +0300

    tak_je_nihua_ne_rabotaet

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,7 @@ Vagrant.configure("2") do |config|
     nginx1.vm.box = "ubuntu/impish64"
     nginx1.vm.provision "shell", path: "nginx1.sh", run: 'always'
     nginx1.vm.network "private_network", ip: "172.30.1.1"
-    nginx1.vm.network "forwarded_port", guest: 80, host: 8080, id:"nginx1"
+    nginx1.vm.network :forwarded_port, guest: 80, host: 8080, id:"nginx1"
     nginx1.vm.synced_folder ".", "C:\VB\test"
 
   end
@@ -15,7 +15,7 @@ Vagrant.configure("2") do |config|
     nginx2.vm.box = "ubuntu/impish64"
     nginx2.vm.provision "shell", path: "nginx2.sh", run: 'always'
     nginx2.vm.network "private_network", ip: "172.30.2.1"
-    nginx2.vm.network "forwarded_port", guest: 80, host: 8081, id:"nginx2"
+    nginx2.vm.network :forwarded_port, guest: 80, host: 8081, id:"nginx2"
     nginx2.vm.synced_folder ".", "C:\VB\test"
 
   end
@@ -25,7 +25,7 @@ Vagrant.configure("2") do |config|
     nginx_proxy.vm.provision "shell", path: "nginx_proxy.sh", run: 'always'
     nginx_proxy.vm.network "private_network", ip: "172.30.1.2"
     nginx_proxy.vm.network "private_network", ip: "172.30.2.2"
-    nginx_proxy.vm.network "forwarded_port", guest: 80, host: 8082, id:"nginx_proxy"
+    nginx_proxy.vm.network :forwarded_port, guest: 80, host: 8082, id:"nginx_proxy"
     nginx_proxy.vm.synced_folder ".", "C:\VB\test"
 
   end
--- nginx_proxy.sh
+++ nginx_proxy.sh
@@ -2,4 +2,40 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 service nginx start
 
-#sudo cat << EOF > /var/www/html/index.nginx-debian.html
+sudo cat << EOF > /etc/nginx/nginx.conf
+user       kuro; 
+worker_processes  1;
+error_log  logs/error.log;
+pid        logs/nginx.pid;
+worker_rlimit_nofile 8192;
+http {
+  include    conf/mime.types;
+  include    /etc/nginx/proxy.conf;
+  include    /etc/nginx/fastcgi.conf;
+  index    index.html index.htm index.php;
+}
+    server {
+    listen        80;
+    server_name _;
+    location / {
+        proxy_pass         http://localhost:8080;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade $http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto $scheme;
+    }
+    location / {
+        proxy_pass         http://localhost:8081;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade $http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto $scheme;
+    }
+}
+EOF
