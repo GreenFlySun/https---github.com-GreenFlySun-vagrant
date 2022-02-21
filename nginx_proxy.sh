05175c18b781d422e840a8f4cb7e9ded4cbee31d
Kuro <tolikzaa2000@gmail.com>
2022-02-21 19:25:13 +0300

    proxy_balanse

--- nginx_proxy.sh
+++ nginx_proxy.sh
@@ -3,39 +3,17 @@ sudo apt-get -y install nginx
 service nginx start
 
 sudo cat << EOF > /etc/nginx/nginx.conf
-user       kuro; 
-worker_processes  1;
-error_log  logs/error.log;
-pid        logs/nginx.pid;
-worker_rlimit_nofile 8192;
-http {
-  include    conf/mime.types;
-  include    /etc/nginx/proxy.conf;
-  include    /etc/nginx/fastcgi.conf;
-  index    index.html index.htm index.php;
+upstream backend_servers {
+    server 172.30.1.1:8080 weight=1;
+    server 172.30.2.1:8081 weight=1;
 }
     server {
     listen        80;
     server_name _;
     location / {
-        proxy_pass         http://localhost:8080;
-        proxy_http_version 1.1;
-        proxy_set_header   Upgrade $http_upgrade;
-        proxy_set_header   Connection keep-alive;
-        proxy_set_header   Host $host;
-        proxy_cache_bypass $http_upgrade;
-        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto $scheme;
-    }
-    location / {
-        proxy_pass         http://localhost:8081;
-        proxy_http_version 1.1;
-        proxy_set_header   Upgrade $http_upgrade;
-        proxy_set_header   Connection keep-alive;
-        proxy_set_header   Host $host;
-        proxy_cache_bypass $http_upgrade;
-        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto $scheme;
+        proxy_pass http://backend_servers;
     }
 }
 EOF
+
+service nginx reload
