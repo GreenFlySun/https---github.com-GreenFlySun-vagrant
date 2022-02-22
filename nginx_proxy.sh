bc723576d4f6715c7f9259aaed0741a72e7be76e
Kuro <tolikzaa2000@gmail.com>
2022-02-22 15:43:02 +0300

    bladstvo

--- nginx_proxy.sh
+++ nginx_proxy.sh
@@ -2,13 +2,13 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 service nginx start
 
-sudo cat << EOF > /etc/nginx/nginx.conf
+sudo cat << EOF > /etc/nginx/sites-enabled/nginx.conf
 upstream backend_servers {
-    server 172.30.1.1:8080 weight=1;
-    server 172.30.2.1:8081 weight=1;
+    server 172.30.1.1:80 weight=1;
+    server 172.30.2.1:80 weight=1;
 }
     server {
-    listen        80;
+    listen        8082;
     server_name _;
     location / {
         proxy_pass http://backend_servers;
